4a106f3ea15679728ff4885ea89080ade557f359
SammyK4488 <skleant1@students.towson.edu>
2025-07-07 06:08:01 -0400

    Update firebase.ts

--- decentrarent-frontend/src/firebase.ts
+++ decentrarent-frontend/src/firebase.ts
@@ -1,3 +1,4 @@
+// src/firebase.ts
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 
@@ -5,10 +6,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyA1w0OUjDEzWYuDmwFCocDQKh_hxhp3haY",
   authDomain: "decentrarent.firebaseapp.com",
   projectId: "decentrarent",
-  storageBucket: "decentrarent.firebasestorage.app",
-  messagingSenderId: "747874582706",
   appId: "1:747874582706:web:ae176702bcd8e704a109ab",
-  measurementId: "G-FXCGJ798NP"
 };
 
 const app = initializeApp(firebaseConfig);

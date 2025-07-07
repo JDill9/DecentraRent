702812ee3e2953907dc99b56b1138b1e4e2c9097
SammyK4488 <skleant1@students.towson.edu>
2025-07-07 00:53:27 -0400

    Update main.tsx

--- decentrarent-frontend/src/main.tsx
+++ decentrarent-frontend/src/main.tsx
@@ -6,10 +6,10 @@ import { AuthProvider } from "./context/AuthContext";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter>
     <AuthProvider>
+      <BrowserRouter>
         <App />
+      </BrowserRouter>
     </AuthProvider>
-    </BrowserRouter>
   </React.StrictMode>
 );
